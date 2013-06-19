0a3710ec71d552ed291e79376d3ec0c6960861fe
Felipe Bazzarella <fbazzarella@gmail.com>
2013-06-19 15:21:30 -0300

    remove location from brazilian

--- app/models/brazilian.rb
+++ app/models/brazilian.rb
@@ -1,5 +1,2 @@
 class Brazilian < User
-  def location
-    [city, region, country].reject { |v| v.blank? }.join(' - ')
-  end
 end
--- spec/models/brazilian_spec.rb
+++ spec/models/brazilian_spec.rb
@@ -1,9 +1,4 @@
 require 'spec_helper'
 
 describe Brazilian do
-  describe '#location' do
-    let!(:brazilian) { FactoryGirl.create(:brazilian, city: 'New York', region: nil, country: 'United States') }
-
-    it { expect(brazilian.location).to be_eql('New York - United States') }
-  end
 end
