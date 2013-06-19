af87dc1bdfef98d4632b12d1c055eb181b7a46bf
Felipe Bazzarella <fbazzarella@gmail.com>
2013-06-18 22:30:32 -0300

    trying to fix config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,8 @@ Brasil::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :less
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
