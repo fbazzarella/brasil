b8659e3008d94d9eaa57462dc54bb4cc23bd4c16
Felipe Bazzarella <fbazzarella@gmail.com>
2013-06-18 22:54:29 -0300

    preventing app initializatio on assets precompile

--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,9 @@ module Brasil
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Prevent initializing your application and connecting to the database
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
