ff1267a2c4b462786932195a6ec8745a6312e299
Felipe Bazzarella <fbazzarella@gmail.com>
2013-06-18 22:34:17 -0300

    and then... finally!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,9 +12,7 @@ Brasil::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
   config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :less
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
