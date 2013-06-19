4c9963a266e223badaa75ce85fe39396fc7ef469
Felipe Bazzarella <fbazzarella@gmail.com>
2013-06-18 21:52:09 -0300

    improve specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 
-# Dir[File.expand_path('../support/*', __FILE__)].each { |f| require f }
+Dir[File.expand_path('../support/*', __FILE__)].each { |f| require f }
 
 RSpec.configure do |config|
-  # config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = true
   config.color_enabled = true
 end
