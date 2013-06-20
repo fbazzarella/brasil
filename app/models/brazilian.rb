edd26d38189deb7f469e9462b81514e0d85e48a5
Felipe Bazzarella <fbazzarella@gmail.com>
2013-06-19 22:46:20 -0300

    scope to show recent brazilians

--- app/models/brazilian.rb
+++ app/models/brazilian.rb
@@ -1,6 +1,8 @@
 class Brazilian < User
   TOTAL_PEOPLE_TO_SHOW = 30
 
+  scope :recent, -> { order('created_at DESC').limit(TOTAL_PEOPLE_TO_SHOW) }
+
   def self.total_other_people
     self.count - TOTAL_PEOPLE_TO_SHOW
   end
--- spec/models/brazilian_spec.rb
+++ spec/models/brazilian_spec.rb
@@ -1,6 +1,17 @@
 require 'spec_helper'
 
 describe Brazilian do
+  describe 'scope .recent' do
+    let!(:people_count) { described_class::TOTAL_PEOPLE_TO_SHOW }
+
+    before do
+      people_count.times { FactoryGirl.create(:brazilian) }
+    end
+
+    it { expect(described_class.recent.count).to be_eql(people_count) }
+    it { expect(described_class.recent.first).to be_eql(described_class.last) }
+  end
+
   describe '.count' do
     let!(:people_count) { described_class::TOTAL_PEOPLE_TO_SHOW + 1 }
 
