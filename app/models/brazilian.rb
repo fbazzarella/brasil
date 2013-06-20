285f808a70ca3d408097d6dfc624eeee5c18ca32
Felipe Bazzarella <fbazzarella@gmail.com>
2013-06-19 22:31:37 -0300

    add total_other_people

--- app/models/brazilian.rb
+++ app/models/brazilian.rb
@@ -1,4 +1,10 @@
 class Brazilian < User
+  TOTAL_PEOPLE_TO_SHOW = 30
+
+  def self.total_other_people
+    self.count - TOTAL_PEOPLE_TO_SHOW
+  end
+
   def first_name
     name.split.first
   end
--- spec/models/brazilian_spec.rb
+++ spec/models/brazilian_spec.rb
@@ -1,6 +1,14 @@
 require 'spec_helper'
 
 describe Brazilian do
+  describe '.count' do
+    let!(:people_count) { described_class::TOTAL_PEOPLE_TO_SHOW + 1 }
+
+    before { people_count.times { FactoryGirl.create(:brazilian) } }
+
+    it { expect(described_class.total_other_people).to be_eql(people_count - described_class::TOTAL_PEOPLE_TO_SHOW) }
+  end
+
   describe '#first_name' do
     let(:brazilian) { FactoryGirl.create(:brazilian) }
 
