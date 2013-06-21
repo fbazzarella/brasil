ac8d625312f3e67c3548a5432a002a1f6a7f00bd
Felipe Bazzarella <fbazzarella@gmail.com>
2013-06-21 14:16:56 -0300

    loading entire list

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,14 +13,40 @@
 //= require jquery
 //= require jquery.tipsy
 
+var renderBrazilians = function(container, brazilians) {
+  var c = container.empty();
+
+  for(i = 0; i < brazilians.length; i++){
+    var b = brazilians[i];
+    var t = brazilianTemplate(b.first_name, b.who_is, b.photo_url);
+    c.append(t);
+  };
+};
+
+var brazilianTemplate = function(firstName, whoIs, photoUrl){
+  return '<li><a class="brazilian" title="' + whoIs + '"><img src="' + photoUrl + '" alt="Foto do perfil de ' + firstName + '" /></a></li>'
+};
+
+var loadTipsy = function(element){
+  element.tipsy({gravity: 's', opacity: 1});
+};
+
 $(function(){
-  $('.brazilian, .logout a').tipsy({
-    gravity: 's',
-    opacity: 1
-  });
+  loadTipsy($('.brazilian, .logout a'));
 
   $('.scroller').click(function(){
     $('html, body').animate({ scrollTop: $('.disclaimer').offset().top - 110 }, 700);
     return false;
   });
+
+  $('.entire-list').click(function(){
+    $.getJSON('/pages.json', function(brazilians){
+      renderBrazilians($('.brazilians'), brazilians);
+      loadTipsy($('.brazilian'));
+    });
+  });
+
+  $(document)
+    .ajaxStart(function(){ $('.entire-list').removeAttr('href').text('Carregando lista completa...') })
+    .ajaxStop(function(){ $('.total-other-people, .entire-list').remove() });
 });
