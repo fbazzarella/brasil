fc5d8c6314cd92ac08f7606f9fcf6b16dfee6afb
Felipe Bazzarella <fbazzarella@gmail.com>
2013-09-28 13:58:12 -0300

    small refactory to add more security

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Brasil::Application.config.secret_token = '7d0e3e8248af02aab76dff5344d3c4171826279898f5516b3fa235f2f5050de5570ad7cd9a1b0f3b78522c69c8037f0c2ff267feadc2f108708457797c892b91'
+Brasil::Application.config.secret_token = ENV['SECRET_TOKEN'] || 'e8aadaec4c3d8af70da46c9ca45c6351'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Brasil::Application.config.session_store :cookie_store, key: '_Brasil_session'
+Brasil::Application.config.session_store :cookie_store, key: '_brasil_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
